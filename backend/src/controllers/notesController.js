bdb10d2a0cb96e504bd67b96d58c96308c4d7d0f
Suprim-Maharjan <suprimmaharjan1921@gmail.com>
2026-05-27 20:21:59 +0545

    Added the rate limiter using the redis database

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -1,9 +1,9 @@
 import Note from "../models/Note.js";
 
 // get all notes controller
-export const getAllNotes = async (req, res) => {
+export const getAllNotes = async (_, res) => {
   try {
-    const notes = await Note.find();
+    const notes = await Note.find().sort({createdAt: -1});
     res.status(200).json(notes);
   } catch (error) {
     console.log("Error in getAllNotes controller", error);
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,23 @@
-import express, { json } from "express"
+import express, { json } from "express";
 import notesRouter from "./routes/notesRoutes.js";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import rateLimiter from "./middlewares/rateLimiter.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
 
 //middleware
-app.use(express.json());
+app.use(express.json()); // this middleware will parse JSON bodies: req.body
+
+app.use(rateLimiter);
+
 app.use("/api/notes", notesRouter);
 
-connectDB();
-
-app.listen(PORT, ()=> {
-    console.log(`Server started on PORT: ${PORT}`)
-})
+// connect to database
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server started on PORT: ${PORT}`);
+  });
+});
